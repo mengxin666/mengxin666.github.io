86f5be39acfb5b86b65799a1e921ce2046f9a721
mengxin666 <64799530+mengxin666@users.noreply.github.com>
2020-05-05 02:34:45 +0800

    测试

--- app-v2.js
+++ app-v2.js
@@ -136,7 +136,7 @@ function list_files(path,files){
         if(item['mimeType'] == 'application/vnd.google-apps.folder'){
             html +=`<li class="mdui-list-item mdui-ripple"><a href="${p}" class="folder">
 	            <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
-	            <i class="mdui-icon material-icons">folder_open</i>
+	            <i class="mdui-icon material-icons">&#xe2c7;</i>
 	              ${item.name}
 	            </div>
 	            <div class="mdui-col-sm-3 mdui-text-right">${item['modifiedTime']}</div>
