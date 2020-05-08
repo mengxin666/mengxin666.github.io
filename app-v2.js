e07ae4da27f5717183eca0e766c63bc8ee656a82
mengxin666 <64799530+mengxin666@users.noreply.github.com>
2020-05-08 12:41:49 +0800

    Update app-v2.js

--- app-v2.js
+++ app-v2.js
@@ -143,7 +143,7 @@ function list(path){
     $.post(path,'{"password":"'+password+'"}', function(data,status){
         var obj = jQuery.parseJSON(data);
         if(typeof obj != 'null' && obj.hasOwnProperty('error') && obj.error.code == '401'){
-            var pass = prompt("目录加密，请输入密码","");
+            var pass = prompt("网盘加密,请输入密码(〃'▽'〃)","");
             localStorage.setItem('password'+path, pass);
             if(pass != null && pass != ""){
                 list(path);
