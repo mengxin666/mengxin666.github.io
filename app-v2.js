dfdc1cc2fbac54eca9c760c865ede3e9acca0823
mengxin666 <64799530+mengxin666@users.noreply.github.com>
2020-05-08 15:11:29 +0800

    Update app-v2.js

--- app-v2.js
+++ app-v2.js
@@ -24,8 +24,21 @@ function init(){
     <header class="nav">
 		<div class="navSize">
 			<a href="/"><img class="avatar" src="http://q1.qlogo.cn/g?b=qq&nk=2013642315&s=640"/></a>
-			<a class="login" href="https://drive.google.com/" target="_blank">登陆</a>
-		</div>
+			<div id="tp-weather-widget"></div>
+  <script>
+    (function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
+    window.SeniverseWeatherWidget('show', {
+      flavor: "slim",
+      location: "WX4FBXXFKE4F",
+      geolocation: true,
+      language: "zh-Hans",
+      unit: "c",
+      theme: "auto",
+      token: "8d842141-ac46-43f7-8ac0-14398f02a562",
+      hover: "enabled",
+      container: "tp-weather-widget"
+    })
+  </script>
 	</header>
 <div class="mdui-container">
 	<div id="nav" class="mdui-toolbar zgh-item"></div>
